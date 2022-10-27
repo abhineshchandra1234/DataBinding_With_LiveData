ad774fc34d2ba134b8dd6bf32207fbe826f9aaa8
abhineshchandra1234 <abhineshchandra1234@gmail.com>
2022-10-27 22:17:42 +0530

    added two way data binding example

--- app/src/main/java/com/example/databindingwithlivedata/MainViewModel.kt
+++ app/src/main/java/com/example/databindingwithlivedata/MainViewModel.kt
@@ -1,13 +1,11 @@
 package com.example.databindingwithlivedata
 
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MainViewModel: ViewModel() {
 
-    private val profileObject = MutableLiveData("Android Developer")
-    val profileLiveData: LiveData<String> get() = profileObject
+    val profileObject = MutableLiveData("Android Developer")
 
     fun updateProfile() {
         profileObject.value = "Full Stack Developer"
